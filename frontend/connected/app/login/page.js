620b5bad484ebd40dab471683a00e733e740d1e9
sheinaedeline <sheina.edeline@live.com>
2023-10-08 00:32:19 +1100

    user type page

--- frontend/connected/app/login/page.js
+++ frontend/connected/app/login/page.js
@@ -16,49 +16,49 @@ export default function Login() {
                 </h2>
                 <form className="space-y-6" action="#" method="POST">
                     <div>
-                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                        Username or Email address
-                    </label>
-                    <div className="mt-2">
-                        <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        autoComplete="email"
-                        required
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            Username or Email address
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
                     </div>
 
                     <div>
-                    <div className="flex items-center justify-between">
-                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                        Password
-                        </label>
-                        <div className="text-sm">
-                        <a href="#" className="font-semibold text-blue-600 hover:text-blue-500">
-                            Forgot password?
-                        </a>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                Password
+                            </label>
+                            <div className="text-sm">
+                                <a href="#" className="font-semibold text-blue-600 hover:text-blue-500">
+                                    Forgot password?
+                                </a>
+                            </div>
                         </div>
-                    </div>
-                    <div className="mt-2">
-                        <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        autoComplete="current-password"
-                        required
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
+                        <div className="mt-2">
+                            <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            autoComplete="current-password"
+                            required
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
                     </div>
 
                     <div>
                         <Link href="/home">
                             <button
                                 type="submit"
-                                className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                                className="flex w-full justify-center rounded-md bg-blue-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                             >
                                 Log in
                             </button>
@@ -77,7 +77,7 @@ export default function Login() {
                 <p className="mt-8 text-center text-sm text-gray-500 pb-8">
                     Don't have an account with us?{' '}
                     <a href="/registration" className="font-semibold leading-6 text-blue-600 hover:text-blue-500">
-                    Sign up
+                        Sign up
                     </a>
                 </p>
             </div>
