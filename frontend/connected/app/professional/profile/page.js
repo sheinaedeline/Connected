8b10c4913bec5a1d38c15c9b3369489c168906eb
sheinaedeline <sheina.edeline@live.com>
2023-10-08 03:38:34 +1100

    profile pages, need to fix header

--- frontend/connected/app/professional/profile/page.js
+++ frontend/connected/app/professional/profile/page.js
@@ -1,3 +1,253 @@
-export default function Page() {
-    return <h1>Professional profile</h1>
+import logo from "assets/Logo Expanded.png"
+import profile from "assets/Profile Icon.png"
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function ProfessionalProfile() {
+
+    return (
+        <div className="bg-white dark:bg-black">
+            <div className="flex justify-between">
+                <Link href="/">
+                    <Image
+                        src={logo}
+                        width={150}
+                        alt="connected logo"
+                    />
+                </Link>
+                <div className="flex justify-between">
+                    {/* Search Bar */}
+                    <button
+                        type="submit"
+                        className="flex w-full justify-center rounded-md bg-blue-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                    >
+                        Create Account
+                    </button>
+                    {/* My Projects */}
+                    <Link href="/professional">
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-blue-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                        >
+                            My Projects
+                        </button>
+                    </Link> 
+                    {/* Logout */}
+                    <Link href="/">
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-blue-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                        >
+                            Logout
+                        </button>
+                    </Link> 
+                    {/* Profile Icon */}
+                    <Link href="/professional/profile">
+                        <Image
+                            src={profile}
+                            width={38}
+                            alt="connected logo"
+                        />
+                    </Link>
+                </div>
+            </div>
+
+            <div className="flex flex-col justify-center px-32">
+                {/* Page Title */}
+                <h2 className="my-4 text-3xl font-bold leading-9 tracking-tight text-gray-900">
+                    My Profile
+                </h2>
+
+                {/* Profile Picture */}
+                <div className="flex gap-4 items-center">
+                    <Image
+                        src={profile}
+                        width={100}
+                        alt="connected logo"
+                    />
+                    {/* Upload Profile Picture Button */}
+                    <button
+                        type="submit"
+                        className="h-8 items-center flex justify-center rounded-md bg-blue-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                    >
+                        Upload Profile Picture
+                    </button>
+                </div>
+
+                {/* Account Credentials */}
+                <div className="mt-12 grid grid-cols-2 gap-4">
+                    <div>
+                        <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-gray-900">
+                            First Name
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="firstName"
+                                name="firstName"
+                                type="text"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-gray-900">
+                            Last Name
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="lastName"
+                                name="lastName"
+                                type="text"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            Email
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="phoneNumber" className="block text-sm font-medium leading-6 text-gray-900">
+                            Phone Number
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="phoneNumber"
+                                name="phoneNumber"
+                                type="text"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                            Username
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="username"
+                                name="username"
+                                type="text"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                            Password
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                {/* Change Password Button */}
+                <div className="flex justify-end m-4">
+                    <button
+                        type="submit"
+                        className="flex justify-center rounded-md bg-blue-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                    >
+                        Change Password
+                    </button>
+                </div>
+
+
+                {/* Personal Details */}
+                <div className="mt-12 grid grid-cols-2 gap-4">
+                    <div>
+                        <label htmlFor="DOB" className="block text-sm font-medium leading-6 text-gray-900">
+                            Date of Birth
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="DOB"
+                                name="DOB"
+                                type="date"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="industryType" className="block text-sm font-medium leading-6 text-gray-900">
+                            Industry Type
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="industryType"
+                                name="industryType"
+                                type="text"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div className="col-span-2">
+                        <label htmlFor="linkedIn" className="block text-sm font-medium leading-6 text-gray-900">
+                            Linkedin Link
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="linkedIn"
+                                name="linkedIn"
+                                type="url"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div className="col-span-2">
+                        <label htmlFor="address" className="block text-sm font-medium leading-6 text-gray-900">
+                            Address
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="address"
+                                name="address"
+                                type="text"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div className="col-span-2">
+                        <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">
+                            Brief Description
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="description"
+                                name="description"
+                                type="text"
+                                required
+                                className="block w-full rounded-md border-0 py-12 mb-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
