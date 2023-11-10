c46aed01d57d15971b48d54c7a06f65527bbe59a
riora-pacil <satriosya@gmail.com>
2023-11-10 17:10:49 +1100

    olibrown

--- frontend/connected/app/projects/page.js
+++ frontend/connected/app/projects/page.js
@@ -157,25 +157,28 @@ export default function Projects() {
                   <div className="grid grid-cols-4 gap-x-10">
                   {filteredProjectList.length > 0  && filteredProjectList.filter(item => (item.status === 'new' || item.status === 'ongoing') && (selectedOptions.length === 0 || selectedOptions.some(opt => item.tags.includes(opt)))).map((item) => (                  
                         <a key={item.id} href={`/project/${item.id}`} className="group rounded-md border-2 border-blue-900 w-[300px] h-[400px] inline-block m-4 cursor-pointer hover:scale-105 ease-in-out duration-300">
-                          <div className="aspect-h-1 aspect-w-1  h-[200px] overflow-hidden xl:aspect-h-8 xl:aspect-w-7">
-                            <Image
-                              src={item.imageSrc}
-                              alt={item.imageAlt}
-                              width={300}
-                              height={200}
-                              className="object-cover object-center group-hover:opacity-75"
-                            />
-                          </div>
-                          <div className="grid grid-cols-2 gap-2 p-4">
+                        <div className="aspect-h-1 aspect-w-1  h-[200px] overflow-hidden xl:aspect-h-8 xl:aspect-w-7">
+                          <Image
+                            src={item.imageSrc}
+                            alt={item.imageAlt}
+                            width={300}
+                            height={200}
+                            className="object-cover object-center group-hover:opacity-75"
+                          />
+                        </div>
+                        <div className="grid grid-cols-2 gap-2 p-4">
                             <p className="col-span-2 text-lg font-bold text-gray-900">{item.project_title}</p>
-                            <p className="col-span-2 text-xs italic text-gray-600">{item.start_date}</p>
-                            <p className="col-span-2 text-xs italic text-gray-600">{item.end_date}</p>
+                            <p className="col-span-2 text-xs text-gray-600 truncate">
+                            Start Date: {new Date(item.start_date).toLocaleDateString()}
+                            </p>
+                            <p className="col-span-2 text-xs text-gray-600 truncate">
+                            End Date: {new Date(item.end_date).toLocaleDateString()}
+                            </p>
                             <p className="col-span-2 text-sm font-medium text-blue-900">{item.owner.userName}</p>
-                            <p className="mt-1 text-sm font-medium text-gray-600">{item.price_budget}</p>
-                            <p className="mt-1 text-sm text-right text-gray-600">{item.industry}</p>
+                            <p className="mt-1 text-sm font-medium text-gray-600">${item.price_budget}/hour</p>
                             <p className="col-span-2 text-xs text-gray-600 truncate">{item.description}</p>
-                          </div>
-                        </a>
+                        </div>
+                      </a>
                       ))}
                   </div>
                 </div>
